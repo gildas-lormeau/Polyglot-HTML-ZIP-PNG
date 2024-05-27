046213af2bf6043068d19c6678c038dc2a1765c8
Gildas <gildas.lormeau@gmail.com>
2024-05-28 01:31:06 +0200

    add result file

---
+++ steps/final/scripts/lib/html-template.js
@@ -0,0 +1,28 @@
+import {
+    minifyScript
+} from "./utils.js";
+import {
+    readTextFile
+} from "./utils-fs.js";
+
+const ZIP_JS_SCRIPT = await readTextFile("assets/zip.min.js");
+const MAIN_SCRIPT = await readTextFile("assets/page-extraction.js");
+
+const template = [`<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset=windows-1252>
+        <title>Please wait...</title>
+        <script>${ZIP_JS_SCRIPT}</script>
+        <script>zip.configure({ useWebWorkers: false })</script>
+    </head>
+    <body>
+        <p>Please wait...</p>
+        <!--`, /* PNG image data chunks */ `-->
+        <!--`, /* ZIP data */ `-->
+        <script type=text/json>`, /* Extra data */ `</script>
+        <script type=module>${minifyScript(MAIN_SCRIPT)}</script>
+    </body>
+</html>`];
+
+export default template;
