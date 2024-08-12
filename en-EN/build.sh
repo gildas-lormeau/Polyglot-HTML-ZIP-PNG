2e051aefc4d002a36d1436e1260c6be85d670e4e
Gildas <gildas.lormeau@gmail.com>
2024-08-12 23:33:00 +0200

    improve readability

--- en-EN/build.sh
+++ en-EN/build.sh
@@ -30,7 +30,12 @@ npx vite build --mode production ./code/16/
 npx vite build --mode production ./code/final/
 
 cp index.html ./build/index.html
-node -e "require('fs').writeFileSync('./build/index.html', require('fs').readFileSync('./build/index.html', 'utf-8').replace(/<code data-src=\"([^\"]+)\"([^>]*)><\/code>/g, (_, src, attrs) => '<code' + attrs + '>' + require('fs').readFileSync(src, 'utf-8').replace(/</g, '&lt;').replace(/>/g, '&gt;') + '</code>'))"
+node -e "
+const fs = require('fs');
+const oldHtml = fs.readFileSync('./build/index.html', 'utf-8');
+const newHtml = oldHtml.replace(/<code data-src=\"([^\"]+)\"([^>]*)><\/code>/g, (_, src, attrs) => '<code' + attrs + '>' + fs.readFileSync(src, 'utf-8').replace(/</g, '&lt;').replace(/>/g, '&gt;') + '</code>');
+fs.writeFileSync('./build/index.html', newHtml);
+"
 
 npx single-file --dump-content --save-raw-page --remove-hidden-elements=false --compress-HTML=false --block-scripts=false --remove-unused-styles=false --remove-unused-fonts=false --remove-alternative-fonts=false --remove-alternative-medias=false --remove-alternative-images=false --compress-content=true --self-extracting-archive=true --insert-single-file-comment=false ./build/index.html > result.html
 
