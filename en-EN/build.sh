a6ee118f16a2b462ef51b7cfd455ec4ce4da2126
Gildas <gildas.lormeau@gmail.com>
2024-08-12 19:53:52 +0200

    process page directly from teh filesytem

--- en-EN/build.sh
+++ en-EN/build.sh
@@ -32,9 +32,7 @@ npx vite build --mode production ./code/final/
 cp index.html ./build/index.html
 node -e "require('fs').writeFileSync('./build/index.html', require('fs').readFileSync('./build/index.html', 'utf-8').replace(/<code data-src=\"([^\"]+)\"([^>]*)><\/code>/g, (_, src, attrs) => '<code' + attrs + '>' + require('fs').readFileSync(src, 'utf-8').replace(/</g, '&lt;').replace(/>/g, '&gt;') + '</code>'))"
 
-npx http-server ./build -p 8080 &
-npx single-file --dump-content --save-raw-page --remove-hidden-elements=false --compress-HTML=false --block-scripts=false --remove-unused-styles=false --remove-unused-fonts=false --remove-alternative-fonts=false --remove-alternative-medias=false --remove-alternative-images=false --compress-content=true --self-extracting-archive=true --insert-single-file-comment=false http://localhost:8080 > result.html
-pgrep http-server | xargs kill -9
+npx single-file --dump-content --save-raw-page --remove-hidden-elements=false --compress-HTML=false --block-scripts=false --remove-unused-styles=false --remove-unused-fonts=false --remove-alternative-fonts=false --remove-alternative-medias=false --remove-alternative-images=false --compress-content=true --self-extracting-archive=true --insert-single-file-comment=false ./build/index.html > result.html
 
 rm -rf ./build
 mkdir ./dist
